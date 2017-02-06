6a84765fce51d06ded5d1ee14e0736b40ee1dbe4
Henrik Urlund <henrik@urlund.com>
2017-02-06 09:56:48 +0100

    gofmt + bugfix

--- types/func.go
+++ types/func.go
@@ -2,57 +2,61 @@ package types
 
 // ...
 import (
-    "strings"
-    "bytes"
-    "os/exec"
-    "syscall"
+	"bytes"
+	"os/exec"
+	"strings"
+	"syscall"
 )
 
 // ...
 func getIndexValue(parts []string, index int, defaultValue string) string {
-    x, v := index, defaultValue
-	if len(parts)-1 >= x  {
+	x, v := index, defaultValue
+	if len(parts)-1 >= x {
 		v = parts[x]
 	}
 
-    return strings.Trim(v, " ")
+	return strings.Trim(v, " ")
 }
 
 // ...
 func commandExists(name string) string {
-    stdout, exitCode := commandExecute("which", name)
-    if exitCode > 0 {
-        return ""
-    }
+	stdout, exitCode := commandExecute("which", name)
+	if exitCode > 0 {
+		return ""
+	}
 
-    return strings.TrimSuffix(string(stdout), "\n")
+	return strings.TrimSuffix(string(stdout), "\n")
 }
 
 // ...
 func commandExecute(name string, arg ...string) (stdout string, exitCode int) {
-    var outbuf bytes.Buffer
+	var outbuf bytes.Buffer
 
-    // ...
-    cmd := exec.Command(name, arg...)
-    cmd.Stdout = &outbuf
+	// ...
+	cmd := exec.Command(name, arg...)
+	cmd.Stdout = &outbuf
 
-    // ...
-    err := cmd.Run()
-    stdout = outbuf.String()
+	// ...
+	err := cmd.Run()
 
-    if err != nil {
-        // try to get the exit code
-        if exitError, ok := err.(*exec.ExitError); ok {
-            ws := exitError.Sys().(syscall.WaitStatus)
-            exitCode = ws.ExitStatus()
-        } else {
-            exitCode = 1
-        }
-    } else {
-        // success, exitCode should be 0 if go is ok
-        ws := cmd.ProcessState.Sys().(syscall.WaitStatus)
-        exitCode = ws.ExitStatus()
-    }
+	// ...
+	if err != nil {
+		stdout = ""
 
-    return stdout, exitCode
+		// try to get the exit code
+		if exitError, ok := err.(*exec.ExitError); ok {
+			ws := exitError.Sys().(syscall.WaitStatus)
+			exitCode = ws.ExitStatus()
+		} else {
+			exitCode = 1
+		}
+	} else {
+		stdout = outbuf.String()
+
+		// success, exitCode should be 0 if go is ok
+		ws := cmd.ProcessState.Sys().(syscall.WaitStatus)
+		exitCode = ws.ExitStatus()
+	}
+
+	return stdout, exitCode
 }
